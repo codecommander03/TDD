06c6ef98524f701bc975706c46ffca3da39d9825
Aditya <codecommander03@gmail.com>
2026-08-07 22:19:24 +0530

    error

--- src/dp/stone_game.cpp
+++ src/dp/stone_game.cpp
@@ -1,13 +1,13 @@
 #include "dp/stone_game.hpp"
 
-int check(int l, int r, vector<int>& p, vector<vector<int>>&dp){
+int check(int l, int r, std::vector<int>& p, std::vector<std::vector<int>>&dp){
         if(l>r) return 0;
         if(dp[l][r] != -1) return dp[l][r];
-        return dp[l][r] = max(p[l]-check(l+1, r, p, dp), p[r]-check(l, r-1, p, dp));
+        return dp[l][r] = std::max(p[l]-check(l+1, r, p, dp), p[r]-check(l, r-1, p, dp));
 
     }
-bool stoneGame(vector<int>& piles) {
+bool stoneGame(std::vector<int>& piles) {
     int n = piles.size();
-    vector<vector<int>> dp(n,vector<int>(n, -1));
+    std::vector<std::vector<int>> dp(n,std::vector<int>(n, -1));
     return check(0, n-1, piles, dp); 
 }
